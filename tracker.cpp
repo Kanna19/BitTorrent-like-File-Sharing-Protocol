200ea17c366ee5efdbc7888567b815855ab196f8
Kanna19 <kannasasuke19@gmail.com>
2019-04-03 21:02:36 +0530

    Tracker creates server threads which accept connections

--- tracker.cpp
+++ tracker.cpp
@@ -3,9 +3,13 @@
 #include <sys/types.h>      // socket()
 #include <sys/socket.h>     // socket()
 #include "helper_functions.h"
+#include <pthread.h>
+
+void* serverWorker(void*);
 
 int main(int argc, char* argv[])
 {
+    // Check if arguments are valid
     if(argc < 2)
     {
         printf("Usage %s <Port Number>\n", argv[0]);
@@ -16,6 +20,8 @@ int main(int argc, char* argv[])
     short listenPort = atoi(argv[1]);
     // Number of clients which can queue up
     const int QUEUED_LIMIT = 10;
+    // Number of server threads
+    const int THREAD_COUNT = 4;
 
     // Create listening socket and set to listen
     int listeningSocketFD = createTCPSocket();
@@ -26,6 +32,41 @@ int main(int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
+    pthread_t threadID[THREAD_COUNT];
+    for(int i = 0; i < THREAD_COUNT; i++)
+        pthread_create(&threadID[i], NULL, serverWorker, (void*)&listeningSocketFD);
+
+    for(int i = 0; i < THREAD_COUNT; i++)
+        pthread_join(threadID[i], NULL);
+
     closeSocket(listeningSocketFD);
     return 0;
+}
+
+void* serverWorker(void* arg)
+{
+    printf("Server Thread %lu created\n", pthread_self());
+    int listeningSocketFD = *(static_cast <int*> (arg));
+
+    while(true)
+    {
+        sockaddr_in clientAddr;
+        unsigned clientLen = sizeof(clientAddr);
+        memset(&clientAddr, 0, clientLen);
+
+        int sockFD = accept(listeningSocketFD,
+                            (sockaddr*) &clientAddr,
+                            (socklen_t*)&clientLen);
+        if(sockFD < 0)
+        {
+            perror("Accept failed");
+            closeSocket(listeningSocketFD);
+            pthread_exit(NULL);
+        }
+
+        // Serve client here
+        closeSocket(sockFD);
+    }
+
+    return NULL;
 }
